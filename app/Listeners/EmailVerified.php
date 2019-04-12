81b165339e329928ab18fa15cf3c79b2bac94f6f
vagrant <vagrant@homestead>
2019-04-12 07:03:23 +0000

    邮箱认证后的消息提醒

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EmailVerified
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Verified  $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        // 会话里闪存认证成功后的消息提醒
+        session()->flash('success', '邮箱认证成功！');
+    }
+}
