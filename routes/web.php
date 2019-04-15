cbcf06749cef2d07e5f41f9240d1b8e3fbf87667
vagrant <vagrant@homestead>
2019-04-15 09:28:24 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
+// 用户的个人中心页面
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
