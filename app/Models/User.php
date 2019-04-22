6a72308e59b8b2c6830526c4170f42f0dca63d6c
vagrant <vagrant@homestead>
2019-04-22 03:25:53 +0000

    上传头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction',
+        'name', 'email', 'password', 'introduction', 'avatar',
     ];
 
     /**
