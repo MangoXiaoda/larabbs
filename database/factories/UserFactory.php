a13d6ad2c144df2b957fd0fe1caf5afb5cc33025
vagrant <vagrant@homestead>
2019-05-16 07:20:11 +0000

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,14 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
+    $date_time = $faker->date . ' ' . $faker->time;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => Str::random(10),
+        'introduction' => $faker->sentence(),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
