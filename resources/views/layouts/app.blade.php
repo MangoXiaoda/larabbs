0ca1b346caff0ef31aa0267db8556ab9d737b51c
vagrant <vagrant@homestead>
2019-06-05 08:34:32 +0000

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
  <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+  @yield('styles')
+
 </head>
 
 <body>
@@ -34,6 +36,8 @@
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+
+  @yield('scripts')
 
 </body>
 
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,62 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+  <div class="container">
     <div class="col-md-10 offset-md-1">
-        <div class="card ">
-            <div class="card-body">
-                <h2 class="">
-                    <i class="far fa-edit">
-                    </i>
-                    @if($topic->id)
+      <div class="card ">
+
+        <div class="card-body">
+          <h2 class="">
+            <i class="far fa-edit"></i>
+            @if($topic->id)
             编辑话题
             @else
             新建话题
             @endif
-                </h2>
-                <hr>
-                    @if($topic->id)
-                    <form accept-charset="UTF-8" action="{{ route('topics.update', $topic->id) }}" method="POST">
-                        <input name="_method" type="hidden" value="PUT">
-                            @else
-                            <form accept-charset="UTF-8" action="{{ route('topics.store') }}" method="POST">
-                                @endif
-                                <input name="_token" type="hidden" value="{{ csrf_token() }}">
-                                    @include('shared._error')
-                                    <div class="form-group">
-                                        <input class="form-control" name="title" placeholder="请填写标题" required="" type="text" value="{{ old('title', $topic->title ) }}"/>
-                                    </div>
-                                    <div class="form-group">
-                                        <select class="form-control" name="category_id" required="">
-                                            <option disabled="" hidden="" selected="" value="">
-                                                请选择分类
-                                            </option>
-                                            @foreach ($categories as $value)
-                                            <option value="{{ $value->id }}">
-                                                {{ $value->name }}
-                                            </option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <textarea class="form-control" id="editor" name="body" placeholder="请填入至少三个字符的内容。" required="" rows="6">
-                                            {{ old('body', $topic->body ) }}
-                                        </textarea>
-                                    </div>
-                                    <div class="well well-sm">
-                                        <button class="btn btn-primary" type="submit">
-                                            <i aria-hidden="true" class="far fa-save mr-2">
-                                            </i>
-                                            保存
-                                        </button>
-                                    </div>
-                                </input>
-                            </form>
-                        </input>
-                    </form>
-                </hr>
-            </div>
+          </h2>
+
+          <hr>
+
+          @if($topic->id)
+            <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
+              <input type="hidden" name="_method" value="PUT">
+          @else
+            <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
+          @endif
+
+              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+              @include('shared._error')
+
+              <div class="form-group">
+                <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required />
+              </div>
+
+              <div class="form-group">
+                <select class="form-control" name="category_id" required>
+                  <option value="" hidden disabled selected>请选择分类</option>
+                  @foreach ($categories as $value)
+                  <option value="{{ $value->id }}">{{ $value->name }}</option>
+                  @endforeach
+                </select>
+              </div>
+
+              <div class="form-group">
+                <textarea name="body" class="form-control" id="editor" rows="6" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
+              </div>
+
+              <div class="well well-sm">
+                <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2" aria-hidden="true"></i> 保存</button>
+              </div>
+            </form>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
+
+@section('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+  <script>
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+      });
+    });
+  </script>
+@stop
