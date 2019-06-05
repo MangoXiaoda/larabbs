25aea556ead35437309d7082f2c15dbc6c7264d5
vagrant <vagrant@homestead>
2019-06-05 10:03:25 +0000

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 // 分类列表话题
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+// 图片上传路由
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
