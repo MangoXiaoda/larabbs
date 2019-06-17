9ce0489a6f7a905887b62943889c4281f3691a13
vagrant <vagrant@homestead>
2019-06-17 07:08:55 +0000

    回复列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -47,17 +47,21 @@
                 <div class="card-body">
                     <ul class="nav nav-tabs">
                         <li class="nav-item">
-                            <a class="nav-link active bg-transparent" href="#">
+                            <a class="nav-link bg-transparent {{ active_class(if_query('tab', null)) }}" href="{{ route('users.show', $user->id) }}">
                                 Ta 的话题
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}" href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">
                                 Ta 的回复
                             </a>
                         </li>
                     </ul>
-                    @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    @if (if_query('tab', 'replies'))
+          @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+        @else
+          @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+        @endif
                 </div>
             </div>
         </hr>
