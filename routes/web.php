6f4652ec5005b61390c14315578a3dc6f279b91c
vagrant <vagrant@homestead>
2019-06-18 02:48:14 +0000

    用户可以发表评论

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,4 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 
 
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
