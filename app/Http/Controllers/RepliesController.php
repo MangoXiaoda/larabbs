8546a7a2a2677e15767e5cfcebd1a030bd3bca69
vagrant <vagrant@homestead>
2019-06-18 07:10:46 +0000

    消息通知有新回复

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -19,6 +19,12 @@ class RepliesController extends Controller
 	public function store(ReplyRequest $request, Reply $reply)
 	{
 
+        // XSS 过滤
+        $content = clean($request->get('content'));
+        if (empty($content)) {
+            return redirect()->back()->with('danger', '回复内容错误！');
+        }
+
 		$reply->content = $request->content;
         $reply->user_id = Auth::id();
         $reply->topic_id = $request->topic_id;
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -23,6 +24,9 @@ class ReplyObserver
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
 }
