275622380657f39fc1466c66c086e925133a8748
vagrant <vagrant@homestead>
2019-07-04 02:48:07 +0000

    侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
 
-    public function show(Category $category, Request $request, Topic $topic)
+    public function show(Category $category, Request $request, Topic $topic, User $user, Link $link)
     {
         // 读取分类 ID 关联的话题， 并按每 20 条分页
         $topics = $topic->withOrder($request->order)
@@ -20,7 +21,10 @@ class CategoriesController extends Controller
         // 获取活跃用户数
         $active_users = $user->getActiveUsers();
 
+        // 资源链接
+        $links = $link->getAllCached();
+
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
---
+++ app/Models/Link.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cache;
+
+class Link extends Model
+{
+    // 允许入库的字段
+    protected $fillable = ['title', 'link'];
+
+    public $cache_key = 'larabbs_links';
+    protected $cache_expire_in_minutes = 1440;
+
+    // 获取缓存数据
+    public function getAllCached()
+    {
+        // 尝试从缓存中取出 cache_key 对应的数据。 如果能取到，便直接返回数据
+        // 否则运行匿名函数中的代码来取出 links 表中所有的数据，返回的同时做了缓存
+        return Cache::remember($this->cache_key, $this->cache_expire_in_minutes, function(){
+            return $this->all();
+        });
+
+    }
+
+}
