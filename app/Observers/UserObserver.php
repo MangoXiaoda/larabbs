632d308c79e68bec9b920babb33c79d94e084273
vagrant <vagrant@homestead>
2019-07-04 06:52:13 +0000

    完善项目

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,13 @@ class UserObserver
     {
         //
     }
+
+
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才能指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
